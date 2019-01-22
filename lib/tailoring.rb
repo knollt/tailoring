f1828ba5838e89a7214bd660569a03dbba93e879
Tim Knoll <timothy.knoll@gmail.com>
2019-01-22 04:47:47 +0000

    updated setup

--- lib/tailoring.rb
+++ lib/tailoring.rb
@@ -2,7 +2,4 @@
 
 require "tailoring/version"
 
-module Tailoring
-  class Error < StandardError; end
-  # Your code goes here...
-end
+
--- lib/tailoring/version.rb
+++ lib/tailoring/version.rb
@@ -1,3 +1,6 @@
+# ignore created for gem file
+
 module Tailoring
   VERSION = "0.1.0"
 end
+
