60982ed4acc2447d42126d4b4529277792da5572
Tim Knoll <timothy.knoll@gmail.com>
2019-01-29 02:45:22 +0000

    brands update

--- lib/tailoring/brands.rb
+++ lib/tailoring/brands.rb
@@ -1,5 +1,24 @@
 class Tailoring::Brands
 
+attr_accessor :desc, :name
 
+  @@all = []
+
+  def self.brand_list(brd)
+    self.new(
+      brd.css("h2").text,
+      
+      )
+  end  
+  
+  def initialize(name=nil, desc=nil)
+    @name = name 
+    @desc = desc 
+  end
+  
+  def self.all
+    @@all
+  end 
+  
   
 end 
