724d79c2518eff6590c9ee005e38935e22c77418
Tim Knoll <timothy.knoll@gmail.com>
2019-01-30 02:21:39 +0000

    scrap update

--- lib/tailoring/scraper.rb
+++ lib/tailoring/scraper.rb
@@ -2,9 +2,10 @@ class Tailoring::Scraper
 
   def self.scrap_brands(url)
     # open url and scrap brands
-    puts "in scraper class"
+    
     # index_page = Nokogiri::HTML(open(https://www.thetrendspotter.net/best-suit-brands-men/))
-    # binding.pry 
+    webpage = Nokogiri::HTML(open(url))
+    binding.pry 
   end
   
   def scrap_desc
