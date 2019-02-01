858d314931db9f0c8a946b57efb1deab35ad6100
Tim Knoll <timothy.knoll@gmail.com>
2019-02-01 02:19:19 +0000

    update brands.rb format

--- lib/tailoring/brands.rb
+++ lib/tailoring/brands.rb
@@ -4,6 +4,12 @@ attr_accessor :desc, :name
 
   @@all = []
 
+  def initialize(name=nil, desc=nil)
+    @name = name 
+    @desc = desc
+    @@all << self #saving/remembering object
+  end
+  
   def self.brand_list(brd)
     self.new(
       brd.css("h2").text,
@@ -11,10 +17,7 @@ attr_accessor :desc, :name
       )
   end  
   
-  def initialize(name=nil, desc=nil)
-    @name = name 
-    @desc = desc 
-  end
+
   
   def self.all
     @@all
